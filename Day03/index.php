3002fe2f7ff1d99732aa9e4a1df573780524285e
JennieOliveira99 <jennyferoliveira7315@gmail.com>
2024-03-31 00:16:55 -0300

    add exercicios restantes

--- Day03/index.php
+++ Day03/index.php
@@ -1,31 +1,19 @@
 <?php
 
-//Desenvolva um programa PHP que determine se um ano é bissexto ou não e imprima o resultado.
-
-$ano = 2000;
-$primeira_condicao = $ano % 4;
-$segunda_condicao = $ano % 400;
-$terceira_condicao = $ano % 100;
-
-
-if ($primeira_condicao == 0 && $terceira_condicao >= 1){
-    echo "Esse ano é bissexto";
-} elseif ($segunda_condicao == 0){
-    echo "Esse ano é bissexto";
-}
-else{
-    echo "Esse ano nao e bissexto";
-}
 
 
 //Elabore um programa PHP que verifique se uma senha digitada pelo usuário é forte o suficiente. Considere que uma senha forte deve ter no mínimo 8 caracteres e conter pelo menos uma letra maiúscula, uma letra minúscula e um número.
 
-//Desenvolva um script PHP que compare duas variáveis numéricas e imprima a maior delas.
 
-//Crie um programa PHP que determine se um número é divisível por 3 ou 5 ou ambos e exiba uma mensagem correspondente.
 
 //Escreva um script PHP que verifique se um caractere digitado pelo usuário é uma vogal ou uma consoante e imprima o resultado.
 
 //Desenvolva um programa PHP que determine se um aluno foi aprovado em uma disciplina com base na sua nota. Considere que a nota mínima para aprovação é 7.
 
-//Crie um script PHP que verifique se um ano fornecido pelo usuário é um ano bissexto ou não.
+$nota = 8;
+ 
+if ($nota >= 7){
+    echo "aprovado";
+}else {
+   echo "reprovado";
+}
--- Day03/teste.php
+++ Day03/teste.php
@@ -22,16 +22,6 @@ if($verificacao == 0){
     echo "Esse numero é negativo";
 }
 
-//Desenvolva um programa PHP que determine se um ano é bissexto ou não e imprima o resultado.
-
-$ano = 2013;
-$anobissexto = 2013 % 4;
-
-if ($anobissexto == 0){
-    echo "Esse ano é bissexto";
-} else{
-    echo "Esse ano nao e bissexto";
-}
 
 
 //Crie um script PHP que determine se uma pessoa pode votar com base na sua idade. Considere que no Brasil, a idade mínima para votar é 16 anos.
@@ -42,4 +32,59 @@ if ($idade >= 16){
    echo "voce pode votar";
 }else{
     echo "voce não pode votar";
+}
+
+
+//Desenvolva um programa PHP que determine se um ano é bissexto ou não e imprima o resultado.
+
+$ano = 2000;
+$primeira_condicao = $ano % 4;
+$segunda_condicao = $ano % 400;
+$terceira_condicao = $ano % 100;
+
+
+if ($primeira_condicao == 0 && $terceira_condicao >= 1){
+    echo "Esse ano é bissexto";
+} elseif ($segunda_condicao == 0){
+    echo "Esse ano é bissexto";
+}
+else{
+    echo "Esse ano nao e bissexto";
+}
+
+//Crie um programa PHP que determine se um número é divisível por 3 ou 5 ou ambos e exiba uma mensagem correspondente.
+$numero = 45;
+$divisivel_por_tres = $numero % 3;
+$divisivel_por_cinco = $numero % 5;
+
+if ($divisivel_por_cinco == 0 && $divisivel_por_tres == 0){
+    echo "Essse número é divisível por 3 e por 5";
+} elseif ($divisivel_por_cinco == 0){
+    echo "Esse numero e divisivel por 5 ";
+}elseif ($divisivel_por_tres == 0){
+    echo "Esse numero e divisivel por 3 ";
+} else {
+   echo  "Esse numero nao e divisivel por 3 nem por 5 ";
+}
+
+//Desenvolva um script PHP que compare duas variáveis numéricas e imprima a maior delas.
+
+$numero_um = 56;
+$numero_dois = 430;
+
+if ($numero_um > $numero_dois){
+    echo "O numero " . $numero_um . " é maior";
+}elseif ($numero_dois > $numero_um){
+    echo "O numero " . $numero_dois . " é maior";
+} else {
+    echo "Não tem numero  maior";
+}
+//Desenvolva um programa PHP que determine se um aluno foi aprovado em uma disciplina com base na sua nota. Considere que a nota mínima para aprovação é 7.
+
+$nota = 8;
+ 
+if ($nota >= 7){
+    echo "aprovado";
+}else {
+   echo "reprovado";
 }
